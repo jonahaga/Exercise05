9ea7d654d35f5d018a0a97a8d2ab09ada36a4db6
Jona Haga-Camacho <jona.haga@gmail.com>
2014-02-15 18:29:44 -0800

    Counts each letter in a text file and prints an alphabetized list with number of frequency.

---
+++ lettercount.py
@@ -0,0 +1,38 @@
+# open a file on command line
+# count how many times each letter occurs in that file
+# print the counts to the screen in alphabetical order
+# use a dictionary
+
+# Useful:
+    # string.lower()
+    # ord()
+
+# argv allows you to open a file from the command line
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+filetext = txt.read()
+txt.close()
+
+d = {}
+# filetext.lower()
+
+
+# Print one character at a time to the screen
+for char in filetext:
+    lowercase = char.lower()
+    if ord(lowercase) >= ord('a') and ord(lowercase)<= ord('z'):
+        if d.get(lowercase):
+            d[lowercase] += 1
+        else:
+            d[lowercase] = 1
+
+
+# Sort the returned dict
+# for key in sorted(d.keys()):
+#     print "%s: %d" % (key, d[key])
+
+for key in sorted(d.keys()):
+    print "%d" % (d[key])
